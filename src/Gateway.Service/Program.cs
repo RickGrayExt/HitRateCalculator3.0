910b1c22edd940d9f08695e9bd32e9aa031327f0
RickGrayExt <rick.krcarpets@gmail.com>
2025-08-27 22:46:31 +0000

    errors from previous build

--- src/Gateway.Service/Program.cs
+++ src/Gateway.Service/Program.cs
@@ -1,6 +1,13 @@
 using Yarp.ReverseProxy;
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// Register YARP
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 app.MapReverseProxy();
 app.Run();
+
