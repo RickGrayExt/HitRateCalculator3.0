b4c86733e2837e6012ba57f02ae92c4b71c65a4f
RickGrayExt <rick.krcarpets@gmail.com>
2025-08-27 22:47:06 +0100

    Delete HitRateRepo_Complete (1) directory

--- HitRateRepo_Complete (1)/README.md
+++
@@ -1,7 +0,0 @@
-# HitRate Microservices (Complete)
-
-Run:
-```bash
-docker compose up --build
-```
-Open UI on port 3000. Gateway on 8000.
--- HitRateRepo_Complete (1)/src/Gateway.Service/Program.cs
+++
@@ -1,6 +0,0 @@
-using Yarp.ReverseProxy;
-var b = WebApplication.CreateBuilder(args);
-b.Services.AddReverseProxy().LoadFromConfig(b.Configuration.GetSection("ReverseProxy"));
-var app = b.Build();
-app.MapReverseProxy();
-app.Run();
